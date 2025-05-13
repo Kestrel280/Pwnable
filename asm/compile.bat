ebad36483f9e2515660fcb4d61bf8b4206d9ae4d
Kestrel280 <downey.samp@gmail.com>
2025-05-13 18:55:01 -0400

    asm working toolchain for developing shellcode

--- asm/compile.bat
+++ asm/compile.bat
@@ -2,4 +2,5 @@ echo off
 set asmfile=%1
 nasm -o %asmfile%.o %asmfile%.asm 
 python to_hex.py %asmfile%.o
+objdump -b binary -Mintel,x86-64 -m i386:x86-64 -D %asmfile%.o
 
---
+++ asm/test.asm
@@ -0,0 +1,20 @@
+bits 64
+
+global _start
+section .text
+
+_start:
+    mov     rsi, filename       ; write(*buf*)
+    add     rsi, 45             ; stub is 45 bytes long
+    add     rsi, 0x41414000     ; memory mapped here
+    mov     dl, 10              ; write(*count*)
+    mov     al, 1               ; write syscall
+    mov     dil, 1              ; write(*fd*)
+    syscall
+
+    mov     al, 60              ; set up for exit syscall
+    mov     dil, 0              ; exit code
+    syscall
+
+section .data:
+    filename db "helloworld", 0
