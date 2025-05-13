ec63bbe8f795093f6f2e3656da59f7fd1c1d991d
Kestrel280 <downey.samp@gmail.com>
2025-05-13 19:30:19 -0400

    asm fixes dirty register usage

--- asm/sh.asm
+++ asm/sh.asm
@@ -5,50 +5,50 @@ section .text
 
 _start:
     ; debug print
-    mov     rsi, okay   ; write(*buf*)
+    mov     rsi, okay       ; write(*buf*)
     add     rsi, 0x41414000 ;   memory map location
     add     rsi, 46         ;   stub length + 1
-    mov     dl, 5           ; write(*count*)
-    mov     dil, 1          ; write(*fd*)
-    mov     al, 1           ; write syscall
+    mov     rdx, 5          ; write(*count*)
+    mov     rdi, 1          ; write(*fd*)
+    mov     rax, 1          ; write syscall
     syscall
 
     mov     rdi, filename   ; open(*filename*)
     add     rdi, 0x41414000 ;   memory map location
     add     rdi, 46         ;   stub length + 1
-    mov     sil, 0          ; open(*flags*)
+    mov     rsi, 0          ; open(*flags*)
     xor     rdx, rdx        ; open(*mode*)
-    mov     al, 2           ; open syscall
+    mov     rax, 2          ; open syscall
     syscall                 ; 
-    mov     r9b, al         ; store fd of open file
+    mov     r9, rax         ; store fd of open file
 
     ; debug print
-    mov     rsi, okay   ; write(*buf*)
+    mov     rsi, okay       ; write(*buf*)
     add     rsi, 0x41414000 ;   memory map location
     add     rsi, 46         ;   stub length + 1
-    mov     dl, 5           ; write(*count*)
-    mov     dil, 1          ; write(*fd*)
-    mov     al, 1           ; write syscall
+    mov     rdx, 5          ; write(*count*)
+    mov     rdi, 1          ; write(*fd*)
+    mov     rax, 1          ; write syscall
     syscall
 
-    mov     dil, r9b        ; read(*fd*)
-    mov     dl, 255         ; read(*count*)
+    mov     rdi, r9         ; read(*fd*)
+    mov     rdx, 255        ; read(*count*)
     mov     rsi, 0x41414500 ; read(*buf*)
-    mov     al, 0           ; read syscall
+    mov     rax, 0          ; read syscall
     syscall
-    mov     r10b, al        ; store chars read from read
+    mov     r10, rax        ; store chars read from read
 
-    mov     dl, r10b        ; chars read from read -> input for write
+    mov     rdx, r10        ; chars read from read -> input for write
     mov     rsi, 0x41414500 ; pointer to char buffer to read into
 
-    mov     dil, 1          ; write(*fd*)
-    mov     dl, 255         ; write(*count*)
+    mov     rdi, 1          ; write(*fd*)
+    mov     rdx, 255        ; write(*count*)
     mov     rsi, 0x41414500 ; write(*buf*)
-    mov     al, 1           ; write syscall
+    mov     rax, 1          ; write syscall
     syscall
 
-    mov     al, 60          ; set up for exit syscall
-    mov     dil, 0          ; exit code
+    mov     rax, 60         ; set up for exit syscall
+    mov     rdi, 0          ; exit code
     syscall
 
 section .data:
