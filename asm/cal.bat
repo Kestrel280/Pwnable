8b2ffca8ef8b17a92bd86da99437413f40694747
Kestrel280 <downey.samp@gmail.com>
2025-05-18 10:50:53 -0400

    asm adds some notes

--- asm/cal.bat
+++ asm/cal.bat
@@ -1,4 +1,4 @@
-echo off
+REM echo off
 set asmfile=%1
-nasm -f win64 -o %asmfile%.o %asmfile%.asm 
+nasm -f win64 -gcv8 -o %asmfile%.o %asmfile%.asm 
 link %asmfile%.o /subsystem:console /out:%asmfile%.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
