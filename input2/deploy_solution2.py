32b33ecb4c3ae5ecbb2d86e7446880ef77e8e1fa
Kestrel280 <downey.samp@gmail.com>
2025-05-18 12:06:19 -0400

    input2 infra finished, stage 1 cleared

--- input2/deploy_solution2.py
+++ input2/deploy_solution2.py
@@ -1,6 +1,10 @@
 from paramiko.client import SSHClient, AutoAddPolicy
+import re
 import time
 import threading
+
+ansi_escape_8bit = re.compile(br'(?:\x1B[@-Z\\-_]|[\x80-\x9A\x9C-\x9F]|(?:\x1B\[|\x9B)[0-?]*[ -/]*[@-~])')
+def dec(b): return ansi_escape_8bit.sub(b'', b).decode('ascii', errors='ignore')
 
 print(" --- connecting ---")
 client = SSHClient()
@@ -23,9 +27,10 @@ stdin.flush()
 alldata = ""
 while 'compiled' not in alldata:
     nbytes = len(stdout.channel.in_buffer)
-    data = stdout.read(nbytes).decode('utf-8')
-    print(data, end = '')
-    alldata += data
+    if (nbytes > 0):
+        data = dec(stdout.read(nbytes))
+        print('\t' + data.replace('\n', '\n\t'), end = '')
+        alldata += data
 
 print("\n\n--- running ---\n\n")
 stdin.write("cd ~\n".encode('utf-8'))
@@ -35,9 +40,10 @@ stdin.flush()
 
 while True:
     nbytes = len(stdout.channel.in_buffer)
-    data = stdout.read(nbytes).decode('utf-8')
-    print(data, end = '')
-    alldata += data
+    if (nbytes > 0):
+        data = dec(stdout.read(nbytes))
+        print('\t' + data.replace('\n', '\n\t'), end = '')
+        alldata += data
 
 print("--- done ---")
 
