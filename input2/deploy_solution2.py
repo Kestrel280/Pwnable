9da30dc8182638c2aed0f6fa5ccdc3af0405e559
Kestrel280 <downey.samp@gmail.com>
2025-05-18 11:23:38 -0400

    input2 sets up infra for deplying solution

---
+++ input2/deploy_solution2.py
@@ -0,0 +1,43 @@
+from paramiko.client import SSHClient, AutoAddPolicy
+import time
+import threading
+
+print(" --- connecting ---")
+client = SSHClient()
+client.set_missing_host_key_policy(AutoAddPolicy())
+client.connect('pwnable.kr', port=2222, username="input2", password="guest")
+
+print(" --- opening sftp ---")
+sftp = client.open_sftp()
+sftp.put('solution.c', '/tmp/k280_input2/solution.c')
+sftp.close()
+
+print(" --- compiling solution ---")
+channel = client.invoke_shell()
+stdin = channel.makefile('wb')
+stdout = channel.makefile('rb')
+
+stdin.write("cd /tmp/k280_input2 && gcc -o solve solution.c && echo -e \"compiled\\n\"".encode('utf-8'))
+stdin.flush()
+
+alldata = ""
+while 'compiled' not in alldata:
+    nbytes = len(stdout.channel.in_buffer)
+    data = stdout.read(nbytes).decode('utf-8')
+    print(data, end = '')
+    alldata += data
+
+print("\n\n--- running ---\n\n")
+stdin.write("cd ~\n".encode('utf-8'))
+stdin.flush()
+stdin.write("/tmp/k280_input2/solve\n".encode('utf-8'))
+stdin.flush()
+
+while True:
+    nbytes = len(stdout.channel.in_buffer)
+    data = stdout.read(nbytes).decode('utf-8')
+    print(data, end = '')
+    alldata += data
+
+print("--- done ---")
+
